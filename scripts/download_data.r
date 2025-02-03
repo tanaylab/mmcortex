8005847afe06b547901ba27f331bfc3f1c1df07f
Yonatan Shapira <yonatan1shapira@gmail.com>
2025-02-03 10:53:40 +0200

    add docker download_data script

---
+++ scripts/download_data.r
@@ -0,0 +1,25 @@
+# script for downloading data for reproducing figures of the mmcortex paper
+
+download.file("https://mmcortex.s3.eu-west-1.amazonaws.com/mmcortex.tar.gz", "mmcortex.tar.gz")
+
+system("tar -xvfz mmcortex.tar.gz")
+
+file.remove("mmcortex.tar.gz")
+
+folder_names <- c('figs', 
+                  'scripts', 
+                  'output', 
+                  'output/metacell_model', 
+                  'output/metacell_flow', 
+                  'output/mcatac',
+                  'output/methylation',
+                  'output/hic',
+                  'output/sequence_modeling',
+                  'output/MPRA',
+                  'output/paper_figs')
+
+for (folder in folder_names) {
+  if(!dir.exists(folder)) {
+    dir.create(folder)
+  }
+}
