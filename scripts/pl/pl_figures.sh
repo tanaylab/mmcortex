f49cc4a683326e526216de692c45adc0025e691f
Yonatan Shapira <yonatan1shapira@gmail.com>
2025-03-19 16:40:02 +0200

    add pl_figures.sh and update Dockerfile

---
+++ scripts/pl/pl_figures.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+cd ~/raid/proj/mmcortex
+
+
+Rscript scripts/pl/figures/Fig1.r
+Rscript scripts/pl/figures/FigS1.r
+Rscript scripts/pl/figures/Fig2.r
+Rscript scripts/pl/figures/FigS2.r
+Rscript scripts/pl/figures/Fig3.r
+Rscript scripts/pl/figures/FigS3.r
+Rscript scripts/pl/figures/Fig4.r
+Rscript scripts/pl/figures/FigS4.r
+Rscript scripts/pl/figures/Fig5.r
+Rscript scripts/pl/figures/FigS6.r
+Rscript scripts/pl/figures/Fig6.r
+Rscript scripts/pl/figures/FigS6.r
+Rscript scripts/pl/figures/Fig7.r
+# Rscript scripts/pl/FigS7.r
